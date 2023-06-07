240de1def3bd8f82a432788d36e670f8f250e588
cheahang-99 <100119039+cheahang-99@users.noreply.github.com>
2023-06-07 17:25:23 +0800

    chore: removed console logs

--- src/util/common.js
+++ src/util/common.js
@@ -1,5 +1,4 @@
 function getDropString(guildId, channelId, paycheck) {
-    console.log(paycheck);
     return `[Drop #${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guildId}/${channelId}/${paycheck.saleMessageId}):\nItem Manager: <@${paycheck.sellerId}>\n${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
 }
 
