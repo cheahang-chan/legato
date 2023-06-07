e4315148ccff9439e4d13653fe0cfc80b67b5d6a
cheahang-99 <100119039+cheahang-99@users.noreply.github.com>
2023-06-07 15:00:27 +0800

    fix: missing closing tag for Manager

--- src/util/common.js
+++ src/util/common.js
@@ -1,6 +1,6 @@
 function getDropString(guildId, channelId, paycheck) {
     console.log(paycheck);
-    return `[Drop #${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guildId}/${channelId}/${paycheck.saleMessageId}):\nManager: <@${paycheck.sellerId}\n${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
+    return `[Drop #${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guildId}/${channelId}/${paycheck.saleMessageId}):\nManager: <@${paycheck.sellerId}>\n${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
 }
 
 module.exports.getDropString = getDropString;
