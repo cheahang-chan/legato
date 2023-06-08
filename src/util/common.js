11226d3df5f1eeb38f55712b7d37915b8fbeb85e
cheahang-99 <100119039+cheahang-99@users.noreply.github.com>
2023-06-09 00:28:29 +0800

    fix: getDropString should generate url based on both saleMessageId and dropMessageId

--- src/util/common.js
+++ src/util/common.js
@@ -1,5 +1,8 @@
 function getDropString(guildId, channelId, paycheck) {
-    return `[Drop #${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guildId}/${channelId}/${paycheck.saleMessageId}):\nItem Manager: <@${paycheck.sellerId}>\n${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
+    const messageId = paycheck.saleMessageId ? paycheck.saleMessageId : paycheck.dropMessageId;
+    // TODO: saleMessageId should eventually be null in the future
+
+    return `[Drop #${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guildId}/${channelId}/${messageId}):\nItem Manager: <@${paycheck.sellerId}>\n${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
 }
 
 module.exports.getDropString = getDropString;
