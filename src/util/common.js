e1cb3f08b124bfc0353b2caf76e8529a7189ae69
cheahang-99 <100119039+cheahang-99@users.noreply.github.com>
2023-08-29 21:21:40 +0800

    fix: show manager issues

--- src/util/common.js
+++ src/util/common.js
@@ -1,7 +1,7 @@
 function getDropString(guild, paycheck, showManager = true) {
     const messageId = paycheck.saleMessageId ? paycheck.saleMessageId : paycheck.dropMessageId;
     const channelId = paycheck.saleMessageId ? guild.salesChannelId : guild.dropsChannelId;
-    return `[#${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guild.id}/${channelId}/${messageId}):\n${showManager ?? "Manager: <@${paycheck.sellerId}>\n"}${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
+    return `[#${paycheck.number} - ${paycheck.item} (${paycheck.boss})](https://discord.com/channels/${guild.id}/${channelId}/${messageId}):\n${showManager ? `Manager: <@${paycheck.sellerId}>\n`: ""}${paycheck.split ? `${paycheck.split.toLocaleString()} mesos\n` : ''}`;
 }
 
 module.exports.getDropString = getDropString;
